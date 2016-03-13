d20dfa71f5228ecdc275dea2ad4f4a70f4e02c67
kenny1352 <kennycampbell52@gmail.com>
2016-03-13 02:20:14 +0000

    Updated Sql with user and insert

--- sql_sprint1.sql
+++ sql_sprint1.sql
@@ -1,8 +1,8 @@
-DROP DATABASE IF EXISTS world;
+DROP DATABASE IF EXISTS ratemyhistory;
 CREATE DATABASE  ratemyhistory;
+CREATE USER assist WITH PASSWORD 'assist';
+GRANT ALL PRIVILEGES ON DATABASE ratemyhistory to assist;
 \c ratemyhistory;
-
-
 
 --
 -- Table structure for table events
@@ -11,9 +11,13 @@ CREATE DATABASE  ratemyhistory;
 DROP TABLE IF EXISTS events;
 CREATE TABLE events (
   ID serial NOT NULL,
-  Name varchar(35)  NOT NULL default '',
+  Name varchar(50)  NOT NULL default '',
   Location varchar(35) NOT NULL default '',
   Description varchar(400) NOT NULL default '',
-  year date,
+  Year date,
   PRIMARY KEY  (ID)
 ) ;
+
+Insert INTO events (Name, Location, Description, Year) Values 
+('Something', 'University of Mary Washington', 'stuff stuff and more stuff', '2016-3-12'),
+('Something Else', 'University of Mary Washington again', 'stuff stuff and more stuff', '2016-3-12');
