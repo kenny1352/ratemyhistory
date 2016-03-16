04fd7bb9ac7fa7a4ed6e62afb9f38277e7f36b0e
klyon85 <kjlyon85@gmail.com>
2016-03-16 18:42:40 +0000

    trash, getting new changes

--- project/server.py
+++ project/server.py
@@ -86,4 +86,4 @@ def bootstrap2():
 
 # start the server
 if __name__ == '__main__':
-        app.run(host=os.getenv('IP', '0.0.0.0'), port =int(os.getenv('PORT', 8080)), debug=True)    
+        app.run(host=os.getenv('IP', '0.0.0.0'), port =int(os.getenv('PORT', 8080)), debug=True)
