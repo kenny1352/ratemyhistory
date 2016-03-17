26fe3f9568ce79e1b2c7c4c58fd61bcd590cc2da
klyon85 <kjlyon85@gmail.com>
2016-03-17 00:22:49 +0000

    made changes to sql file to work for passwords being salted, two users test and admin both passwords are their name

--- sql_sprint1.sql
+++ sql_sprint1.sql
@@ -1,8 +1,12 @@
 DROP DATABASE IF EXISTS ratemyhistory;
 CREATE DATABASE  ratemyhistory;
+DROP USER IF EXISTS assist;
 CREATE USER assist WITH PASSWORD 'assist';
 GRANT ALL PRIVILEGES ON DATABASE ratemyhistory to assist;
 \c ratemyhistory;
+
+DROP EXTENSION IF EXISTS pgcrypto;
+CREATE EXTENSION pgcrypto;
 
 --
 -- Table structure for table events
@@ -49,6 +53,7 @@ CREATE TABLE users (
     ID serial NOT NULL,
     Username varchar(20) NOT NULL,
     Email varchar(60) NOT NULL,
+    Password text NOT NULL,
     Firstname varchar(35),
     Lastname varchar(35),
     Company varchar(80),
@@ -60,6 +65,11 @@ CREATE TABLE users (
     Phone varchar(24),
     Fax varchar(24),
     PRIMARY KEY (ID)
+);
+
+INSERT INTO users (Username, Email, Password) VALUES
+('test','test@test.com', crypt('test', gen_salt('bf'))),
+('admin', 'admin@admin.com', crypt('admin', gen_salt('bf')))
 );
 
 GRANT INSERT, UPDATE, SELECT ON ALL TABLES IN SCHEMA public To assist;
